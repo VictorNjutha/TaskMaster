cff238cd10c96b72edef9b7c90a58a5e7d6a1db7
asmaali2 <asma.maalim7@gmail.com>
2024-03-07 10:45:34 +0300

    changes commited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
+import Register from './Register';
+import Login from './Login';
+import Logout from './Logout';
+import Comments from './Comments';
+import Demote from './Demote';
+import GroupForm from './GroupForm';
+import GroupList from './GroupList';
+import GroupUpdate from './GroupUpdate';
+import Profile from './Profile';
+import Promote from './Promote';
+import TaskForm from './TaskForm';
+import TaskList from './TaskList';
+import UpdateForm from './UpdateForm';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <h1>welcome</h1>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/demote" element={<Demote />} />
+          <Route path="/groupform" element={<GroupForm />} />
+          <Route path="/grouplist" element={<GroupList />} />
+          <Route path="/groupupdate" element={<GroupUpdate />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/promote" element={<Promote />} />
+          <Route path="/taskform" element={<TaskForm />} />
+          <Route path="/tasklist" element={<TaskList />} />
+          <Route path="/updateform" element={<UpdateForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
