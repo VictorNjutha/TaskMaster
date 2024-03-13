133fee6c3625da6336031617f9d552c3351aeb4b
+ <+>
2024-03-13 12:28:32 +0300

    resolving merge issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,14 @@
 
-// import './App.css';
+import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//      <h1>welcome</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './NavBar';
-import Register from './Register';
-import Promote from './Promote';
-import TaskForm from './TaskForm';
-import TaskList from './TaskList';
-import UpdateForm from './UpdateForm';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/Register" element={<Register />} />
-          
-          <Route path="/Promote" element={<Promote />} />
-          <Route path="/TaskForm" element={<TaskForm />} />
-          <Route path="/TaskList" element={<TaskList />} />
-          <Route path="/UpdateForm" element={<UpdateForm />} />
-          <Route path="TaskForm" element={<TaskForm />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+     <h1>welcome</h1>
+    </div>
   );
-};
+}
 
 export default App;
+
+
